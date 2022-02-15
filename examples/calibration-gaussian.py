ca3da088b931ddabafb7a4dcd60eac21bbf4f1b6
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2022-02-15 13:32:57 -0500

    gaussian recalibration setup tested both with average and EWA forecasters

--- examples/calibration-gaussian.py
+++ examples/calibration-gaussian.py
@@ -2,17 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from scipy.special import erf
+from scipy.special import erf, erfinv
 from matplotlib import pyplot as plt
-from forecasters.recalibration import EWARecalibratedForecaster
+from forecasters.recalibration import (
+  EWARecalibratedForecaster, EWARecalibratedRegressionForecaster
+)
 from forecasters.calibration import quantile_calib_loss
 
 # We sample y from Bernoulli(p)
 
 # parameters
 mu, sigma = 0.0, 1.0
-T = 4000
-N = 20
+T = 400
+N = 40
 
 # we use the CRPS loss
 def crps_loss(p_vals, y_vals, y_target):
@@ -27,8 +29,9 @@ y_vals = np.arange(-2.0, 2.0, 0.05)
 p_raw_vals = [0.5*(1 + erf((y-mu_raw)/(sigma_raw*np.sqrt(2)))) for y in y_vals]
 
 # construct forecaster
-eta = np.sqrt(2*np.log(N)/T)
-F = EWARecalibratedForecaster(N, eta)
+# eta = np.sqrt(2*np.log(N)/T)
+eta = 5
+F = EWARecalibratedRegressionForecaster(N, eta)
 
 # run experiment
 F_losses = np.zeros(T,)
@@ -39,17 +42,24 @@ P_raw = np.zeros(T,)
 for t in range(T):
   if t % 100 == 0: print(t)
   Y[t] = np.random.normal(mu, sigma)
-  p_raw = 0.5*(1 + erf((Y[t]-mu_raw)/(sigma_raw*np.sqrt(2))))
-  P[t] = F.predict(p_raw)
-  P_exp[t] = F.expected_prediction(p_raw)
-  P_raw[t] = p_raw
+  P_raw[t] = 0.5*(1 + erf((Y[t]-mu_raw)/(sigma_raw*np.sqrt(2))))
+  P[t] = F.predict(P_raw[t])
+  P_exp[t] = F.expected_prediction(P_raw[t])
+
+  print('Y[t]', Y[t])
+  print('P_raw[t]', P_raw[t])
+  print('idx[t]', F._get_idx(P_raw[t]))
+  print('P_t[t]', P[t])
+  print('P_exp[t]', P_exp[t])
+  print('P_correct[t]',0.5*(1 + erf((Y[t]-mu)/(sigma*np.sqrt(2)))))
+  print()
 
   # compute crps loss
   # p_vals = np.array([F.predict(p_raw_val) for p_raw_val in p_raw_vals])
   p_vals = np.array([F.expected_prediction(p_raw_val) for p_raw_val in p_raw_vals])
   F_losses[t] = crps_loss(p_vals, y_vals, Y[t])
 
-  F.observe(Y[t], p_raw)
+  F.observe(Y[t], P_raw[t])
 
 print('test0')
 # plot the expected loss:
@@ -78,7 +88,7 @@ for i in range(1,10):
     p = float(i)/10.
     p_hat = np.sum([1 for p_t in P if p_t <= p]) / T
     w_hat = np.sum([1 for p_t in P if p_t <= p])
-    cal_loss += w_hat * (p_hat - p)**2
+    cal_loss += (p_hat - p)**2
     print(p, p_hat)
 print('Loss: %f %f %f\n' % (cal_loss, quantile_calib_loss(P, 10), cum_cal_loss[-1])) 
 
@@ -88,25 +98,32 @@ for i in range(1,10):
     p = float(i)/10.
     p_hat = np.sum([1 for p_t in P_exp if p_t <= p]) / T
     cal_loss += (p_hat - p)**2
-    print(p, p_hat)
+    print(p, p_hat) # F.F_cal[F._get_idx(p)+1].predict(), F.F_cal[F._get_idx(p)+1].input_history)
 print('Loss: %f\n' % cal_loss) 
 
-print('CRPS Analysis:')
-# compute crps loss
-p_vals = np.array([np.mean(np.array([F.expected_prediction(p_raw_val) for _ in range(1000)]))
-  for p_raw_val in p_raw_vals])
-y_target = Y[t]
-n_vals = len(y_vals)
-crps_vals = (p_vals-np.array(y_vals >= y_target, dtype=np.int))**2
-delta_vals = np.array([y_vals[i+1] - y_vals[i] for i in range(n_vals-1)])
-print([(i, f.examples_seen) for i, f in enumerate(F.F_cal)])
-print(y_vals)
-print(p_raw_vals)
-print(p_vals)
-print(np.array(y_vals >= y_target, dtype=np.int))
-print(crps_vals)
-print(delta_vals)
-# return np.sum(crps_vals[:n_vals-1] * delta_vals)
+print('Recalibration Plot:')
+for i in range(1,10):
+    p_raw = float(i)/10.
+    y_raw = mu_raw + sigma_raw * np.sqrt(2) * erfinv(2*p_raw-1)
+    p_true = 0.5*(1 + erf((y_raw-mu)/(sigma*np.sqrt(2))))
+    p_pred = F.F_cal[F._get_idx(p_raw)+1].predict()
+    print(p_raw, p_pred, p_true)
+
+# print('CRPS Analysis:')
+# # compute crps loss
+# p_vals = np.array([np.mean(np.array([F.expected_prediction(p_raw_val) for _ in range(1000)]))
+#   for p_raw_val in p_raw_vals])
+# y_target = Y[t]
+# n_vals = len(y_vals)
+# crps_vals = (p_vals-np.array(y_vals >= y_target, dtype=np.int))**2
+# delta_vals = np.array([y_vals[i+1] - y_vals[i] for i in range(n_vals-1)])
+# print([(i, f.examples_seen) for i, f in enumerate(F.F_cal)])
+# print(y_vals)
+# print(p_raw_vals)
+# print(p_vals)
+# print(np.array(y_vals >= y_target, dtype=np.int))
+# print(crps_vals)
+# print(delta_vals)
 
 # plt.show()
 plt.savefig('calibration-gaussian.png')
